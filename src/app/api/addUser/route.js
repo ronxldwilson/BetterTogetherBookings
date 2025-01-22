d7245a1348718844a5aaf70b4fe236f401118370
RonxldWilson <ronaldwilson00000@gmail.com>
2025-01-22 21:51:53 +0530

    adding payments gatway v0.4

---
+++ src/app/api/addUser/route.js
@@ -0,0 +1,43 @@
+import { supabase } from '../../../lib/supabase'
+
+export async function POST (req) {
+  try {
+    const body = await req.json()
+
+    const { email, name, phone } = body
+
+    // Validate required fields
+    if (!email || !name || !phone) {
+      return new Response(
+        JSON.stringify({ error: 'Missing required fields' }),
+        {
+          status: 400,
+          headers: { 'Content-Type': 'application/json' }
+        }
+      )
+    }
+
+    // Insert data into the `user` table
+    const { data, error } = await supabase
+      .from('user')
+      .insert([{ email, name, phone }])
+      
+    if (error) {
+      console.error('Supabase Error:', error)
+      throw new Error(error.message)
+    }
+
+    return new Response(
+      JSON.stringify({ message: 'User inserted successfully', data }),
+      {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' }
+      }
+    )
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' }
+    })
+  }
+}
